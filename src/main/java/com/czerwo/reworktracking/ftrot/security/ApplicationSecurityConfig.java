ee69e7544502757aa5655f277906c6b43268efd1
Mateusz Czerwinski <mateczerw@gmail.com>
2021-02-23 22:11:42 +0100

    bug fix in security - logged in user had access to each endpoint

--- src/main/java/com/czerwo/reworktracking/ftrot/security/ApplicationSecurityConfig.java
+++ src/main/java/com/czerwo/reworktracking/ftrot/security/ApplicationSecurityConfig.java
@@ -53,9 +53,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-                .antMatchers("/api/v1/engineer/*").hasRole(ApplicationUserRole.ENGINEER.name())
-                .antMatchers("/api/v1/technical-project-manager/*").hasRole(ApplicationUserRole.TECHNICAL_PROJECT_MANAGER.name())//.hasRole(ApplicationUserRole.TECHNICAL_PROJECT_MANAGER.name())
-                .antMatchers("/api/v1/lead-engineer/*").hasRole(ApplicationUserRole.LEAD_ENGINEER.name())//.hasRole(ApplicationUserRole.LEAD_ENGINEER.name())
+                .antMatchers("/api/v1/engineer/**").hasRole(ApplicationUserRole.ENGINEER.name())
+                .antMatchers("/api/v1/technical-project-manager/**").hasRole(ApplicationUserRole.TECHNICAL_PROJECT_MANAGER.name())
+                .antMatchers("/api/v1/lead-engineer/**").hasRole(ApplicationUserRole.LEAD_ENGINEER.name())
+                .antMatchers("/api/v1/team-leader/**").hasRole(ApplicationUserRole.TEAM_LEADER.name())
                 .anyRequest()
                 .authenticated();
 
